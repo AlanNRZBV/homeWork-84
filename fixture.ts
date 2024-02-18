74fdb78b57b6a4ae0aacd2c0ab69a0838e8748af
Alan Noruzbaev <alan.noruzbaev@gmail.com>
2024-02-18 17:50:24 +0600

    feat: implemented user creation and login

--- fixture.ts
+++ fixture.ts
@@ -1,61 +1,60 @@
-import mongoose from 'mongoose';
-import config from './config';
-import Category from './models/Category';
-import Product from './models/Product';
-import User from './models/User';
-import * as crypto from 'crypto';
-
-const dropCollection = async (db: mongoose.Connection, collectionName: string)=>{
-  try{
-    await db.dropCollection(collectionName);
-  }catch (e){
-    console.log(`Collection ${collectionName} was missing, skipping drop`)
-  }
-}
-
-const run = async()=>{
-  await mongoose.connect(config.mongoose.db);
-  const db = mongoose.connection
-
-  try{
-    const collections = ['categories','products','users']
-
-    for(const collectionName of collections){
-      await dropCollection(db, collectionName)
-    }
-
-    const [cpuCategory, ssdCategory]=await Category.create({
-      title:'CPUs',
-      description:'Central Processing Units',
-    },
-      {
-        title: 'SSDs',
-        description:"Solid State Drivers"
-      })
-
-    await Product.create({
-      title:'Intel Core i7 12700k',
-      price: 350,
-      category: cpuCategory,
-      image:'fixture/cpu.jpg'
-    },{
-      title:'Samsung 990 Pro 1TB',
-      price: 170,
-      category: ssdCategory,
-      image:'fixtures/ssd.jpg'
-    })
-
-    await User.create({
-      username:'user',
-      password:'1@345qWert',
-      token: crypto.randomUUID()
-    })
-
-    await db.close()
-
-  }catch (e){
-    console.log('Collection were missing, skipping drop')
-  }
-}
-
-void run()
+// import mongoose from 'mongoose';
+// import config from './config';
+//
+// import User from './models/User';
+// import * as crypto from 'crypto';
+//
+// const dropCollection = async (db: mongoose.Connection, collectionName: string)=>{
+//   try{
+//     await db.dropCollection(collectionName);
+//   }catch (e){
+//     console.log(`Collection ${collectionName} was missing, skipping drop`)
+//   }
+// }
+//
+// const run = async()=>{
+//   await mongoose.connect(config.mongoose.db);
+//   const db = mongoose.connection
+//
+//   try{
+//     const collections = ['categories','products','users']
+//
+//     for(const collectionName of collections){
+//       await dropCollection(db, collectionName)
+//     }
+//
+//     const [cpuCategory, ssdCategory]=await Category.create({
+//       title:'CPUs',
+//       description:'Central Processing Units',
+//     },
+//       {
+//         title: 'SSDs',
+//         description:"Solid State Drivers"
+//       })
+//
+//     await Product.create({
+//       title:'Intel Core i7 12700k',
+//       price: 350,
+//       category: cpuCategory,
+//       image:'fixture/cpu.jpg'
+//     },{
+//       title:'Samsung 990 Pro 1TB',
+//       price: 170,
+//       category: ssdCategory,
+//       image:'fixtures/ssd.jpg'
+//     })
+//
+//     await User.create({
+//       username:'user',
+//       password:'1@345qWert',
+//       token: crypto.randomUUID()
+//     })
+//
+//     await db.close()
+//
+//   }catch (e){
+//     console.log('Collection were missing, skipping drop')
+//   }
+// }
+//
+// void run()
--- routers/users.ts
+++ routers/users.ts
@@ -1,5 +1,50 @@
 import {Router} from 'express';
+import mongoose from 'mongoose';
+import User from '../models/User';
 
 const usersRouter = Router();
+
+
+usersRouter.post('/',async(req,res,next)=>{
+  try {
+    const user = new User({
+      username: req.body.username,
+      password: req.body.password
+    })
+    user.generateToken();
+    await user.save()
+
+    res.send(user)
+  } catch (e) {
+    next(e)
+    if (e instanceof mongoose.Error.ValidationError) {
+      return res.status(422).send(e)
+    }
+  }
+})
+
+usersRouter.post('/sessions', async (req, res, next) => {
+  try {
+    const user = await User.findOne({username: req.body.username})
+
+    if (!user) {
+      return res.status(422).send({error: 'Username not found!'})
+    }
+
+    const isMatch = await user.checkPassword(req.body.password);
+
+    if (!isMatch) {
+      return res.status(422).send({error: 'Password is wrong'})
+    }
+
+    user.generateToken();
+    await user.save();
+    return res.send({message: 'Username and password is correct!', user})
+
+
+  } catch (e) {
+    next(e)
+  }
+})
 
 export default usersRouter
